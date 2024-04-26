af3d7420a341854b6cbcd5a1f4f14d44f640624e
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-04-27 00:31:41 +0300

    update this headache later

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+import Home from './Home'
+
+import './App.css'
+
+function App() {
+  
+  
+  
+  return (
+    // fragmenting;
+    <>
+    <div className="title">
+      <h1>Dummy Blog</h1>
+    </div>
+    
+    
+    <Home></Home>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react';
+
+const Home = () => {
+
+
+
+    
+    return ( 
+
+     );
+}
+ 
+export default Home;
+
